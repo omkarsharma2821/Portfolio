e8e8a538bac6e101e16c960c5787d8fa66582f93
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-26 09:35:25 +0530

    updated

--- omkar/src/data/constants.js
+++ omkar/src/data/constants.js
@@ -283,7 +283,7 @@ export const projects = [
     title: "PG Life",
     date: "2022",
     description:
-      "Designed and developed the Vexa project, a project management app that helps users and teams stay organized and on track. Implemented key features such as task tracking, team collaboration, and time tracking to improve productivity and project outcomes and also invite team/project members. The plan is to add a community of users where they can find developers and other team members and chat system implementation.",
+      "Designed and developed user friendly PG managing website helpful in finding the nearest PG and Rooms for students. The plan is to add all the affordable rooms and flat to help students.",
     image:
       "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
     tags: [
@@ -303,7 +303,7 @@ export const projects = [
     title: "Movie Review",
     date: "Jun 2021",
     description:
-      "A simple react app that shows the characters of the famous TV series Breaking Bad. It uses the Breaking Bad API to fetch the data. It also has a search bar to search for a particular character.",
+      "A simple website that shows the rating of the famous mavies and allow viewers to rate the movies on their likes and dislikes. The site also has a search bar to search for a particular movie.",
     image:
       "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
     tags: ["React Js", "API", "Axios", "Node JS"],
