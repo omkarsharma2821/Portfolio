f176eb723da428ad962dc496599b71ff7e08f688
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-26 18:50:33 +0530

    avtar

--- omkar/src/data/constants.js
+++ omkar/src/data/constants.js
@@ -266,7 +266,7 @@ export const projects = [
     member: [
       {
         name: "Omkar Sharma",
-        img: "https://avatars.githubusercontent.com/u/108568853?v=4",
+        img: "https://avatars.githubusercontent.com/u/104587783?s=96&v=4",
         linkedin: "https://www.linkedin.com/in/omkarsharmaa/",
         github: "https://github.com/omkarsharma2821/",
       },
@@ -285,7 +285,7 @@ export const projects = [
     description:
       "Designed and developed user friendly PG management website helpful in finding the nearest PG and Rooms for students. The plan is to add all the affordable rooms and flat to help students.",
     image:
-      "https://user-images.githubusercontent.com/64485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      "https://user-images.githubusercontent.com/6485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
     tags: [
       "React Js",
       "MongoDb",
@@ -305,7 +305,7 @@ export const projects = [
     description:
       "A simple website that shows the rating of the famous movies and allow viewers to rate the movies on basis of their likes and dislikes. The site also has a search bar to search for a particular movie.",
     image:
-      "https://camo.githubusercontent.com/cd07010cbeb90cb1b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+      "https://camo.githubusercontent.com/cd07010cbeb901b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
     tags: ["React Js", "API", "Axios", "Node JS"],
     category: "website",
     github: "https://github.com/omkarsharma2821/fullstack-movie-project",
@@ -315,7 +315,7 @@ export const projects = [
     id: 6,
     title: "Todo List",
     description:
-      " A Todo List made with React JS and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+      "A Todo List made with React JS and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
     image:
       "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
     tags: ["React Js", "MongoDB", "User Auth", "Node JS"],
