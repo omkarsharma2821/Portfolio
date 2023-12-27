0d5d0afd93e87fb9dce0b16ea3042be9e5eb7c0d
Omkar Sharma <omkarsharma2821@gmail.com>
2023-12-27 15:19:19 +0530

    changes

--- omkar/src/data/constants.js
+++ omkar/src/data/constants.js
@@ -156,6 +156,7 @@ export const experiences = [
       "Postman",
       "SEO"
     ],
+    // doc:""
   },
   {
     id: 0,
@@ -171,7 +172,7 @@ export const experiences = [
       "JavaScript",
       "GitHub"
     ],
-    // doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+    // doc: "https://drive.google.com/file/d/19dHIaj-lnkeigz6p0hcQklnjPuOT2ts8/view?usp=sharing",
   },
   {
     id: 1,
@@ -186,6 +187,7 @@ export const experiences = [
       "JavaScript",
       "MongoDB",
     ],
+    // doc: "https://drive.google.com/file/d/1iius6V9HRWfHU6lNNbxnc6b23vRrA_rb/view?usp=sharing",
   },
   {
     id: 2,
@@ -193,7 +195,7 @@ export const experiences = [
     role: "Research",
     company: "ICAETC 2023",
     date: "Present",
-    desc: "MobileNet-v3 : A Comprehensive Survey of Object Detection Algorithms Using CNN. International Conference on Advances in Emerging Trends in Computer Applications (ICAETC-2023) MobileNet-v3 used for detection identification of disease in Plants/Crops",
+    desc: "MobileNet-v3 : A Comprehensive Survey of Object Detection Algorithms Using CNN. International Conference on Advances in Emerging Trends in Computer Applications (ICAETC-2023). For my Major Project I with my teammate wrote this Review Paper and used it in the detection identification of disease in Plants/Crops",
     // doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
   }
 ];
@@ -285,7 +287,7 @@ export const projects = [
     description:
       "Designed and developed user friendly PG management website helpful in finding the nearest PG and Rooms for students. The plan is to add all the affordable rooms and flat to help students.",
     image:
-      "https://user-images.githubusercontent.com/6485885/234916413-96296f13-fe4b-4cc4-b215-e72bd7c27928.png",
+      "https://drive.google.com/file/d/1iius6V9HRWfHU6lNNbxnc6b23vRrA_rb/view?usp=sharing",
     tags: [
       "React Js",
       "MongoDb",
@@ -305,7 +307,7 @@ export const projects = [
     description:
       "A simple website that shows the rating of the famous movies and allow viewers to rate the movies on basis of their likes and dislikes. The site also has a search bar to search for a particular movie.",
     image:
-      "https://camo.githubusercontent.com/cd07010cbeb901b43a5d6162784326aef02210ef7d41a0f9ae043b3e392378/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f7425323028313534292e706e673f616c743d6d6564696126746f6b656e3d65613439383630632d303435362d343333342d616435372d336239346663303333363263",
+      "https://drive.google.com/file/d/19dHIaj-lnkeigz6p0hcQklnjPuOT2ts8/view?usp=sharing",
     tags: ["React Js", "API", "Axios", "Node JS"],
     category: "website",
     github: "https://github.com/omkarsharma2821/fullstack-movie-project",
