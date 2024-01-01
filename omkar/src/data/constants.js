ee27ef812cacf8001d5de70f6f1eb23e78b40f8d
Omkar Sharma <omkarsharma2821@gmail.com>
2024-01-01 12:24:43 +0530

    ss

--- omkar/src/data/constants.js
+++ omkar/src/data/constants.js
@@ -341,7 +341,7 @@ export const projects = [
     description:
       "A Todo List made with React JS and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
     image:
-      "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+      "https://raw.githubusercontent.com/omkarsharma2821/Portfolio/main/screenshot/Screenshot%202024-01-01%20122050.png",
     tags: ["React Js", "MongoDB", "User Auth", "Node JS"],
     category: "webpage",
     github: "https://github.com/omkarsharma2821/React/",
